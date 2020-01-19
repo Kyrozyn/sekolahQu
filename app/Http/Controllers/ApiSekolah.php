4146593c228e8e2d58459b9b3bb190eec3988385
kyrozyn <arizkirizaldi@email.unikom.ac.id>
2020-01-19 18:09:12 +0700

    delete example

---
+++ app/Http/Controllers/ApiSekolah.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class ApiSekolah extends Controller
+{
+    function api(){
+        return file_get_contents('http://jendela.data.kemdikbud.go.id/api/index.php/cwilayah/wilayahKabGet');
+    }
+
+    function namaSekolah($kodedaerah){
+        return file_get_contents('http://jendela.data.kemdikbud.go.id/api/index.php/Csekolah/detailSekolahGET?mst_kode_wilayah='.$kodedaerah."&bentuk=sd");
+    }
+}
