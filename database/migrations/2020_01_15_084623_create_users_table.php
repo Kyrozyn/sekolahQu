0a3f76898fb63bafe556d540081c0e124fc085ff
kyrozyn <arizkirizaldi@email.unikom.ac.id>
2020-01-22 23:47:51 +0700

    stabil

--- database/migrations/2020_01_15_084623_create_users_table.php
+++ database/migrations/2020_01_15_084623_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('password');
-            $table->integer('NPSN')->references('NPSN')->on('sekolah');
+            $table->integer('NPSN')->references('NPSN')->on('sekolahs');
         });
     }
 
